a272077875a987c0e2a033ce3cf0abcb6b802929
Zanetkah <32001173+zanetkah@users.noreply.github.com>
2024-01-06 21:58:45 +0100

    Add files via upload

--- web/src/components/client-page/add-client-form/add-client.tsx
+++ web/src/components/client-page/add-client-form/add-client.tsx
@@ -1,7 +1,8 @@
+import FormTextInput from "src/components/shared/form/form-text-input";
 import { Button, Stack, Typography } from "@mui/material";
 import { Form, Formik } from "formik";
 import * as React from "react";
-import FormTextInput from "src/components/shared/form/form-text-input";
+
 
 const AddClientForm: React.FC = () => {
   return (
@@ -13,58 +14,102 @@ const AddClientForm: React.FC = () => {
         sx={{
           marginBottom: "40px",
         }}
-      >
+        >
         Wypełnij formularz, aby dodać klienta
       </Typography>
-      <Formik
-        onSubmit={console.log}
-        initialValues={{
-          name: "",
-          surname: "",
-          phone: "",
-          email: "",
-        }}
-      >
-        <Form>
-          <Stack direction="row" spacing={2}>
+        <Formik
+          onSubmit={console.log}
+          initialValues={{
+            name: "",
+            surname: "",
+            phone: "",
+            email: "",
+            street: "",
+            flatNumber: "",
+            postcode: "",
+            city: "",
+          }}
+        >
+          <Form>
+            <Stack direction="row" spacing={2}>
+              <FormTextInput
+                autoFocus
+                margin="dense"
+                label="Imie"
+                name="name"
+                fullWidth
+              />
+              <FormTextInput
+                autoFocus
+                margin="dense"
+                label="Nazwisko"
+                type="text"
+                name="surname"
+                fullWidth
+              />
+            </Stack>
             <FormTextInput
               autoFocus
               margin="dense"
-              label="Imie"
-              name="name"
-              fullWidth
-            />
-            <FormTextInput
-              autoFocus
-              margin="dense"
-              label="Nazwisko"
+              label="Telefon"
               type="text"
-              name="surname"
+              name="phone"
               fullWidth
             />
-          </Stack>
-          <FormTextInput
-            autoFocus
-            margin="dense"
-            label="Telefon"
-            type="text"
-            name="phone"
-            fullWidth
-          />
-          <FormTextInput
-            autoFocus
-            margin="dense"
-            label="Email"
-            type="email"
-            name="email"
-            fullWidth
-          />
-          <Button type="reset">Odrzuć</Button>
-
-          <Button type="submit">Dodaj klienta</Button>
-        </Form>
-      </Formik>
-    </>
+            <FormTextInput
+              autoFocus
+              margin="dense"
+              label="Email"
+              type="email"
+              name="email"
+              fullWidth
+            />
+            <Stack direction="row" spacing={2}>
+              <FormTextInput
+                autoFocus
+                margin="dense"
+                label="Ulica"
+                type="text"
+                name="street"
+                fullWidth
+              />
+              <FormTextInput
+                autoFocus
+                margin="dense"
+                label="Numer budynku"
+                type="text"
+                name="flatNumber"
+                fullWidth
+              />
+            </Stack>
+            <Stack direction="row" spacing={2}>
+              <FormTextInput
+                autoFocus
+                margin="dense"
+                label="Kod pocztowy"
+                type="text"
+                name="postcode"
+                fullWidth
+              />
+              <FormTextInput
+                autoFocus
+                margin="dense"
+                label="Miejscowość"
+                type="text"
+                name="city"
+                fullWidth
+              />
+            </Stack>
+            <Button type="reset">
+              Odrzuć
+            </Button>
+            
+            <Button type="submit">
+                Dodaj klienta
+            </Button>
+          </Form>
+        </Formik>
+        </>
   );
 };
 
