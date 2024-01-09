7e75b3b8562c5c3c623d69206bc99f7a3d9e6d05
zanetkah <zaneta.hofman@gmail.com>
2024-01-09 14:47:16 +0100

    updated product page

---
+++ web/src/components/product-page/product-row.tsx
@@ -0,0 +1,71 @@
+import { useMutation } from "@tanstack/react-query";
+import { Button, TableRow, TableCell } from "@mui/material";
+import * as React from "react";
+import { UpsertProductPayload, Product, _updateProduct } from "src/api/products";
+import ProductFormDialog from "./product-form-dialog";
+import { VscEdit } from "react-icons/vsc";
+import FormTextInput from "src/components/shared/form/form-text-input";
+
+interface Props {
+    product: Product;
+    allProducts: Product[];
+    refetch: () => Promise<unknown>;
+}
+
+const ProductRow: React.FunctionComponent<Props> = ({ 
+  product,
+  allProducts,
+  refetch, 
+}) => {
+  const [editProductDialogOpen, setEditProductDialogOpen] = React.useState<boolean>(false);
+
+  const { mutateAsync: updateProduct } = useMutation({
+    mutationFn: (values: UpsertProductPayload) => _updateProduct(values, product.id),
+    onSuccess: () => {
+      refetch();
+    },
+  });
+
+  const handleSubmit = async (values: any) => {
+    await updateProduct({
+      name: values.name,
+      amount: values.amount,
+      description: values.description,
+    });
+
+    setEditProductDialogOpen(false);
+  };
+
+  return (
+    <>
+    <TableRow>
+    <TableCell 
+      component="th" 
+      scope="row"
+    >
+      {product.name}
+    </TableCell>
+    <TableCell align="right">{product.amount}</TableCell>
+    <TableCell align="right">
+      <Button
+        color="secondary"
+        onClick={() => {setEditProductDialogOpen(true)}}
+        >
+          <VscEdit />
+      </Button>
+    </TableCell>
+    <ProductFormDialog
+        open={editProductDialogOpen}
+        handleClose={() => setEditProductDialogOpen(false)}
+        product={product}
+        products={allProducts}
+        onSubmit={handleSubmit}
+        title="Edytuj produkt"
+        description="Wypełnij formularz aby edytować produkt"
+        submitButtonLabel="Zapisz"
+      />  
+    </TableRow>
+    </>
+  );
+};
+export default ProductRow;
